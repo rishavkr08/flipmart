23e7edc1a2f32e1122bb2932923dff38fb89e8e3
rishav74492644 <rishav74492644@gmail.com>
2019-07-18 17:15:44 +0530

    Update in order process lifecycle

--- app/controllers/process_orders_controller.rb
+++ app/controllers/process_orders_controller.rb
@@ -2,21 +2,13 @@
 
 class ProcessOrdersController < ApplicationController
   def index
-    user_order_track = {
-      order_processing: :accept,
-      order_accepted: :deliver,
-      out_for_delivery: :delivered,
-      order_delivered: :success
-    }
-    param           = params[:order_status].parameterize.underscore.to_sym
     @orders         = Order.where(aasm_state: params[:order_status]).all
     @order_status   = params[:order_status].humanize
-    @order_process  = user_order_track[param].to_s
+    @order_process  = params[:order_process]
   end
 
   def update
     Order.find(params[:id]).send(params[:order_update].to_s + '!')
-
     # method.().call
     # .try("")
     redirect_back fallback_location: root_path
